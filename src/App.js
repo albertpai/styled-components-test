a42ee297ed3e80742de50ee0ec404b5cfdea67b9
albertpai <verdant.light@gmail.com>
2018-09-04 02:14:47 +0100

    Hello World!

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <HelloWorld/>
+        );
+    }
+}
