8c02cecd1c7e315896ae1cba511b2175d5e1d39b
albertpai <verdant.light@gmail.com>
2018-09-06 00:33:14 +0100

    Improve the counter again

--- src/Counter.js
+++ src/Counter.js
@@ -13,28 +13,34 @@ color: Indigo;
 
 const Button = styled.button`
 font-size: 1em;
-width: 4em;
+width: 5em;
+height: 2em;
 margin: 0 10px;
 `;
 
 export default class Counter extends React.Component {
-    state = {count: 0}
-    increment = () => { this.setState ({count: this.state.count + 1})}
-    decrement = () => { this.setState ({count: this.state.count - 1})}
-    double = () => { this.setState ({count: this.state.count * 2 })}
-    halve = () => { this.setState ({count: this.state.count / 2})}
-    reset = () => { this.setState ({count: 0})}
+    state = { count: 0 }
+    increment = () => { this.setState({ count: this.state.count + 1 }) }
+    decrement = () => { this.setState({ count: this.state.count - 1 }) }
+    double = () => { this.setState({ count: this.state.count * 2 }) }
+    halve = () => { this.setState({ count: this.state.count / 2 }) }
+    negation = () => { this.setState({ count: -(this.state.count) }) }
+    reset = () => { this.setState({ count: 0 }) }
     render() {
         return (
             <StyledCounter>
-                <Paragraph><h2>Press buttons to increment or decrement the counter: {this.state.count}</h2></Paragraph>
-                <Button onClick = {this.increment}>+</Button>
-                <Button onClick = {this.decrement}>-</Button>
-                <Button onClick = {this.double}>*2</Button>
-                <Button onClick = {this.halve}>/2</Button>
-                <br/>
-                <br/>
-                <Button onClick = {this.reset}>Reset</Button>
+                <Paragraph>
+                    <h2>Press buttons to change the counter: </h2>
+                    <h2>{this.state.count}</h2>
+                </Paragraph>
+                <Button onClick={this.increment}>+1</Button>
+                <Button onClick={this.decrement}>-1</Button>
+                <Button onClick={this.double}>×2</Button>
+                <Button onClick={this.halve}>÷2</Button>
+                <Button onClick={this.negation}>Negation</Button>
+                <br />
+                <br />
+                <Button onClick={this.reset}>Reset</Button>
             </StyledCounter>
         )
     }
