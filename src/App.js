2492ef9aa4256cb0e0c6c1ba1ba5cf2930648206
albertpai <verdant.light@gmail.com>
2018-09-05 22:40:31 +0100

    Add Counter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import HelloWorld from './HelloWorld';
 import Counter from './Counter';
 
-
 export default class App extends React.Component {
     render() {
         return (
